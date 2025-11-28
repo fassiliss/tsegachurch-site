0fe81f6ceadac8f7bc6d614debfe61937fc3111d
fassiliss <fassiliss@gmail.com>
2025-11-28 14:53:52 -0600

    Remove collection.css import (missing font dependencies)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import { SessionProvider } from "next-auth/react";
 import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/style.css";
-import "../styles/collection.css";
 import "../styles/custom.css";
 import "../styles/mfp.css";
 import "swiper/css";
