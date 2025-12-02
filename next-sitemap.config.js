829d00afcb2770164220ae44c471ecd1e342b1ab
fassiliss <fassiliss@gmail.com>
2025-12-01 21:59:16 -0600

    next-sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,4 +3,13 @@ module.exports = {
     siteUrl: "https://www.geecn.org",
     generateRobotsTxt: true,
     sitemapSize: 5000,
+    changefreq: "weekly",
+    priority: 0.8,
+    exclude: ["/admin/*", "/api/*", "/private/*"],
+    robotsTxtOptions: {
+        additionalSitemaps: [
+            "https://www.geecn.org/sitemap.xml",
+            "https://www.geecn.org/sitemap-0.xml",
+        ],
+    },
 };
